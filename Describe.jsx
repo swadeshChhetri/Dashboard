7c4d2f463a5e1bda9099fb9b65e284761bb358c5
Swadesh Chhetri <swadeshchhetri87@gmail.com>
2024-11-06 16:08:23 +0530

    I add some sections, fix the layout, & image

---
+++ Describe.jsx
@@ -0,0 +1,44 @@
+/*
+Problem: Creating left-side bar;
+Solutin: 
+step1 : used aside tag with an attribute class="sidebar"
+step2 : 
+
+
+
+
+
+
+
+
+
+
+Probelems: Adding font
+Soluting : we should have to write exact the same name;
+
+
+
+
+
+
+
+*/
+
+/* How did you create a dashboard?
+
+1. create a container using //*div tag & container class for all dashboard;
+2. Inside the container I used aside tag with "sidebar class" to gather all items to the right side.
+     inside the sidebar rule we used some property like - display:flex; flex-direction: column;  justify-content: space-between;
+3. then I created a main section using main tag with main-content class.
+
+
+
+
+
+
+
+
+
+
+
+*/
